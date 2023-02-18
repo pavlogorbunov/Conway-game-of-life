0ca4c3a5bd493f6b98e7b644e1d285912b7a87f3
pavlogorbunov <pavlogorbunov@yandex.ru>
2023-02-19 02:26:08 +0600

    conway 1.3

--- js/script.js
+++ js/script.js
@@ -36,23 +36,42 @@ let playTimer = {};
 
 function handlePlayButtonClick(e) {
     isPlaying = !isPlaying;
-    field.classList.toggle('field_edit');
     if (isPlaying) {
+        field.classList.remove('field_edit');
+        playButton.classList.add('header__button_active');
         playButton.innerText = 'СТОП';
         playTimer = play();
     } else {
+        field.classList.add('field_edit');
+        playButton.classList.remove('header__button_active');
         playButton.innerText = 'СТАРТ';
         stop(playTimer);
     }
 }
 
 function handleClearButtonClick() {
+    if (isPlaying) {
+        field.classList.add('field_edit');
+        playButton.classList.remove('header__button_active');
+        playButton.innerText = 'СТАРТ';
+        isPlaying = false;
+        stop(playTimer);
+    }
     while (field.firstChild) {
         field.removeChild(field.firstChild);
     }
+    handleResize();
+}
+
+function handleResize() {
+    const cellSize = 18;
+    const xCells = Math.floor(field.offsetWidth / cellSize);
+    cellWidth = field.offsetWidth / xCells;
+    const yCells = Math.floor((window.innerHeight - field.getBoundingClientRect().top) / cellSize);
+    cellHeight = (window.innerHeight - field.getBoundingClientRect().top) / yCells;
     B = [];
-    for (let i = 0; i < 33; ++i) {
-        B.push(Array(60).fill(0));
+    for (let i = 0; i < yCells; ++i) {
+        B.push(Array(xCells).fill(0));
     }
     showCells(B);
 }
@@ -69,6 +88,8 @@ function showCells(array) {
             if (el === 1) {
                 cell.classList.add('cell_active');
             };
+            cell.style.height = cellHeight + 'px';
+            cell.style.width = cellWidth + 'px';
             cell.i = i;
             cell.j = j;
             cell.addEventListener('mouseover', handleCellMouseOver);
@@ -80,17 +101,17 @@ function showCells(array) {
 let B = [];
 let isPlaying = false;
 
-for (let i = 0; i < 33; ++i) {
-    B.push(Array(60).fill(0));
-}
+let cellWidth = 18;
+let cellHeight = 18;
 
 const field = document.getElementById('field');
 const playButton = document.getElementById('playButton');
 const clearButton = document.getElementById('clearButton');
 
-showCells(B);
+handleResize();
 
 field.addEventListener('mousedown', handleCellMouseDown);
 playButton.addEventListener('click', handlePlayButtonClick);
 clearButton.addEventListener('click', handleClearButtonClick);
-document.addEventListener('');
+window.addEventListener('resize', () => { setTimeout(handleResize, 200) });
+field.height = window.innerHeight - field.getBoundingClientRect().top;
